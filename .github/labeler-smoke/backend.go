1ebda5a717a457b02c39c0008118cf0d31f0110c
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2026-08-15 00:53:03 -0700

    test: retrigger PR labeler webhook

--- .github/labeler-smoke/backend.go
+++ .github/labeler-smoke/backend.go
@@ -1,2 +1,4 @@
 // Package labelersmoke contains a temporary fixture used to verify PR labeling.
 package labelersmoke
+
+// This comment triggers a pull_request synchronize delivery for the smoke test.
